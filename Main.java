5ac6b8703bfe66750feb3f60e853e71a20ba6e78
Beckett Morsch <brm8633@rit.edu>
2024-01-25 10:04:48 -0500

    Created first code

---
+++ Main.java
@@ -0,0 +1,11 @@
+//Imports
+
+// Main class for the project
+// Authors:
+
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
