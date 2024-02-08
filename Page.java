fd391244617b21aec483af9b42f1f62f40e54009
Leaseback <kmk4427@rit.edu>
2024-02-08 23:05:31 +0800

    Added splitpage functionAdded splitpAdded splitpage function	ge function

--- Page.java
+++ Page.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Page {
     int numRecords;
     ArrayList<Record> records;
+    nextPage = Page;
 
     public Page() {
         this.numRecords = 0;
@@ -11,5 +12,13 @@ public class Page {
 
     public void dropAttribute(String a_name) {
 
+    }
+
+    public ArrayList<Record> splitPage{
+        // Cut array in half, remove second half of values, return to Table class those values
+        int cutoffIndex = record.size() / 2 - 1;
+        ArrayList<Record> page2 = records.sublist(cutOffIndex,records.size()-1);
+        records = records.sublist(0, cutoffIndex);
+        return page2;
     }
 }
