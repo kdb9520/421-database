156b2710b8fdaefc50fbe24eac8152d5fd52aa34
Beckett Morsch <brm8633@rit.edu>
2024-02-13 12:26:03 -0500

    Started work on DML class

--- DMLParser.java
+++ DMLParser.java
@@ -1,7 +1,24 @@
+import java.util.LinkedList;
+
+import javax.management.Query;
+
 public class DMLParser {
 
-    public DMLParser() {
+    BufferManager bufferManager;
 
+    public DMLParser(BufferManager bufferManager) {
+        this.bufferManager = bufferManager;
+    }
+
+    public void handleQuery(String query) {
+
+        if (query.substring(0, 11).equals("insert into ")) {
+            insert(query);
+        }
+
+        if (query.substring(0, 14).equals("display schema ")) {
+            displaySchema(query.substring(14));
+        }
     }
 
     public void insert(String query) {
@@ -12,8 +29,30 @@ public class DMLParser {
 
     }
 
-    private void displaySchema() {
-        
+    private void displaySchema(String query) {
+
+    }
+
+    private boolean displayInfo(String tableName) {
+
+        Table table = bufferManager.getTable(tableName);
+        if (table != null) {
+            // get schema
+            String schema = "";
+
+            int pageNumber = table.numPages;
+
+            LinkedList<Page> pages = table.pages;
+            int numRecords = 0;
+            for (Page page : pages) {
+                numRecords = numRecords + page.numRecords;
+            }
+
+            System.out.println("Table: "+ tableName + "\nSchema: " + schema + "\nNumber of Pages: " + pageNumber + "\nNumber of Records: " + numRecords);
+            return true;
+        }
+
+        return false;
     }
 
     private Boolean checkTypes(String param) {
@@ -35,7 +74,4 @@ public class DMLParser {
         String name = args[2];
     }
 
-    public void alterTable(){
-
-    }
 }
