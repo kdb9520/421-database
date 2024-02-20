ba40d793d1ed1d426eba1adef1b6a7720aaae9c8
Beckett Morsch <brm8633@rit.edu>
2024-02-20 10:42:59 -0500

    Added main handling of no semicolon

--- Main.java
+++ Main.java
@@ -51,6 +51,9 @@ public class Main {
                             commandBuilder.append(line.substring(index + 1).trim()).append("\n");
                         }
                     }
+                    else {
+                        System.out.println("Invalid Command: Semicolon required at the end of each command");
+                    }
                 }
             }
             else {
