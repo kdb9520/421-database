f06c2dc6fc33adeca85e2e022b82d218498f81f1
Kellen Bell <93352579+kdb9520@users.noreply.github.com>
2024-03-21 10:37:23 -0400

    testing different types in the cartesian

--- test/SelectTest.java
+++ test/SelectTest.java
@@ -309,4 +309,77 @@ public class SelectTest {
             fail("Exception Caught!");
         }
     }
+
+    @Test
+    void testCartesianDifferentTypes() {
+        try {
+            tearDown();
+            System.out.println("Testing SELECT * FROM two tables.");
+            setUp();
+
+            String[] args = {"C:\\Users\\kelle\\RIT\\421\\db", "10000", "10000"};
+
+            String input = """
+                    CREATE TABLE test1 (foo VARCHAR(10) PRIMARYKEY, baz DOUBLE);
+                    CREATE TABLE test2 (foo INTEGER PRIMARYKEY);
+                    INSERT INTO test1 VALUES ('Hello' 1.0), ('Bye' 2.1), ('Kellen' 3.0);
+                    INSERT INTO test2 VALUES (4), (5), (6);
+                    SELECT test1.foo, test2.foo FROM test1, test2;
+                    DROP TABLE test1;
+                    DROP TABLE test2;
+                    QUIT;
+                    """;
+            ByteArrayInputStream inputIn = new ByteArrayInputStream(input.getBytes());
+            System.setIn(inputIn);
+
+            Main.main(args);
+
+            String expected = """
+                    > Processed Query: create table test1 (foo varchar(10) primarykey, baz double);
+                    Table created successfully.
+                    > Processed Query: create table test2 (foo integer primarykey);
+                    Table created successfully.
+                    > Processed Query: insert into test1 values ('Hello' 1.0), ('Bye' 2.1), ('Kellen' 3.0);
+                    > Processed Query: insert into test2 values (4), (5), (6);
+                    > Processed Query: select test1.foo, test2.foo from test1, test2;
+                    Table created successfully.
+                    | test1.foo|| test2.foo|
+                    ------------------------
+                    |     "Bye"||         4|
+                    |     "Bye"||         5|
+                    |     "Bye"||         6|
+                    |   "Hello"||         4|
+                    |   "Hello"||         5|
+                    |   "Hello"||         6|
+                    |  "Kellen"||         4|
+                    |  "Kellen"||         5|
+                    |  "Kellen"||         6|
+
+                    Schema removed from src.Catalog
+                    > Processed Query: drop table test1;
+                    Schema removed from src.Catalog
+                    > Processed Query: drop table test2;
+                    Schema removed from src.Catalog
+                    > Processed Query: quit;
+                    Shutting down database...
+                    Shutdown complete.
+                    """;
+            expected = expected.replaceAll("\r", "");
+            String output = outputStreamCaptor.toString().replaceAll("\r", "");
+
+            boolean pass = expected.equals(output);
+
+            if (!pass) {
+                fail("Output does not match expected.");
+            } else {
+                assertEquals(expected, output);
+            }
+
+            tearDown();
+            System.out.println("Captured Output:");
+            System.out.println(output);
+        } catch (Exception e) {
+            fail("Exception Caught!");
+        }
+    }
 }
