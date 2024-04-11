9cf2a73f2891cb6bba408aa63945876cb523cd4d
Derek Pruski <dmp3872@g.rit.edu>
2024-04-11 10:24:32 -0400

    added new implementation

--- src/BPlusTree.java
+++ src/BPlusTree.java
@@ -5,69 +5,33 @@ import java.util.List;
 
 class BPlusTree {
     private Node root;
-    private int maxPageSize;
-    private int maxKeySize;
-    private int maxPointerSize;
-    private int n;
+    private int maxDegree;
 
     // Constructor
-    public BPlusTree(int maxPageSize, int maxKeySize, int maxPointerSize) {
-        this.maxPageSize = maxPageSize;
-        this.maxKeySize = maxKeySize;
-        this.maxPointerSize = maxPointerSize;
-        this.n = (int) (Math.floor(maxPageSize / (maxKeySize + maxPointerSize)) - 1);
-        this.root = new LeafNode();
-    }
-
-    // Search method
-    public Node search(int key) {
-        Node C = root;
-
-
-
-
-        //Traverse the tree until we reach a leaf node
-        while(!(C instanceof LeafNode)){
-            int i = 0;
-            while (i < C.keys.size()){
-                if(key < C.keys.get(i)){
-                    break;
-                }
-                i +=1;
-            }
-            // need child node functionality
-            //C = node.children[i];
-
-
-            i = 0;
-
-            // Now 'node' points to a leaf node, search for the key within this node
-            while(i < C.keys.size()){
-                if (key == C.keys.get(i)){
-                    // return C.values[i];
-
-                }
-                else if (key < C.keys.size()) {
-                    break;
-                }
-
-                i += 1;
-            }
-
-
-        }
-
-        return null;
+    public BPlusTree(int maxDegree) {
+        this.maxDegree = maxDegree;
+        this.root = new Node(true, maxDegree); // Initially, root is a leaf node
     }
 
     // Insert method
     public void insert(int key, Object value) {
-        // Implement insertion logic here
+        root.insert(key, value);
+        if (root.isOverflow()) {
+            Node newRoot = new Node(false, maxDegree); // New root will be an internal node
+            newRoot.addChild(root);
+            root.split(newRoot, 0);
+            root = newRoot;
+        }
+    }
+
+    // Search method
+    public Object search(int key) {
+        return root.search(key);
     }
 
     // Delete method
     public void delete(int key) {
         // Implement deletion logic here
     }
-    
+
 }
