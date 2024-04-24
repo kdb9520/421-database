bf372c15570a4374a6480de93c00e9497a67602e
Kyle Kaczorowski <69587926+Leaseback@users.noreply.github.com>
2024-04-24 17:22:37 -0400

    Test break main

--- src/bplustreedraft/Main.java
+++ src/bplustreedraft/Main.java
@@ -5,29 +5,13 @@ import src.BPlusTree;
 public class Main {
     public static void main(String[] args) {
         bplustreedraft bpt = null;
-    bpt = new bplustreedraft(5);
-    bpt.insert(5, 33);
-    bpt.insert(15, 21);
-    bpt.insert(25, 31);
-    bpt.insert(35, 41);
-    bpt.insert(45, 10);
-    bpt.insert(10, 10); 
-    bpt.insert(12, 10); 
-    bpt.delete(35);
-    bpt.delete(25);
+    
+        bpt = new bplustreedraft(5);
+        for(int i = 0; i < 30; i++){
+            bpt.insert(i, 1);
+        }
+        
+        bpt.print();
 
-    bpt.print();
-
-    if(1==1){
-        return;
-    }
-
-
-    if (bpt.search(15) != null) {
-      System.out.println("Found");
-    } else {
-      System.out.println("Not Found");
-    }
-    ;
   }
 }
